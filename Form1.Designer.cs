9825ad6eaa4f93a492ddbad54c6dddbefe0085fe
Bitrate2001 <tarditi.luca@gmail.com>
2023-07-03 16:04:15 +0200

    Aggiungere i file di progetto.

---
+++ Form1.Designer.cs
@@ -0,0 +1,330 @@
+﻿namespace Codio
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            button1 = new Button();
+            textBox1 = new TextBox();
+            button2 = new Button();
+            button3 = new Button();
+            button4 = new Button();
+            listBox1 = new ListBox();
+            button5 = new Button();
+            label1 = new Label();
+            label2 = new Label();
+            tableLayoutPanel1 = new TableLayoutPanel();
+            tableLayoutPanel2 = new TableLayoutPanel();
+            tableLayoutPanel3 = new TableLayoutPanel();
+            tableLayoutPanel4 = new TableLayoutPanel();
+            button6 = new Button();
+            label3 = new Label();
+            label4 = new Label();
+            numericUpDown1 = new NumericUpDown();
+            numericUpDown2 = new NumericUpDown();
+            tableLayoutPanel1.SuspendLayout();
+            tableLayoutPanel2.SuspendLayout();
+            tableLayoutPanel3.SuspendLayout();
+            tableLayoutPanel4.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)numericUpDown1).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)numericUpDown2).BeginInit();
+            SuspendLayout();
+            // 
+            // button1
+            // 
+            button1.Anchor = AnchorStyles.Top | AnchorStyles.Left | AnchorStyles.Right;
+            button1.Location = new Point(635, 3);
+            button1.Name = "button1";
+            button1.Size = new Size(156, 23);
+            button1.TabIndex = 0;
+            button1.Text = "Submit";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
+            // 
+            // textBox1
+            // 
+            textBox1.Dock = DockStyle.Bottom;
+            textBox1.Location = new Point(3, 66);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(794, 23);
+            textBox1.TabIndex = 1;
+            textBox1.TextChanged += textBox1_TextChanged;
+            // 
+            // button2
+            // 
+            button2.Anchor = AnchorStyles.Top | AnchorStyles.Left | AnchorStyles.Right;
+            button2.Location = new Point(477, 3);
+            button2.Name = "button2";
+            button2.Size = new Size(152, 23);
+            button2.TabIndex = 3;
+            button2.Text = "Cancella tutto";
+            button2.UseVisualStyleBackColor = true;
+            button2.Click += button2_Click;
+            // 
+            // button3
+            // 
+            button3.Anchor = AnchorStyles.Top | AnchorStyles.Left | AnchorStyles.Right;
+            button3.Location = new Point(319, 3);
+            button3.Name = "button3";
+            button3.Size = new Size(152, 23);
+            button3.TabIndex = 4;
+            button3.Text = "Cancella testo sopra";
+            button3.UseVisualStyleBackColor = true;
+            button3.Click += button3_Click;
+            // 
+            // button4
+            // 
+            button4.Anchor = AnchorStyles.Top | AnchorStyles.Left | AnchorStyles.Right;
+            button4.Location = new Point(3, 3);
+            button4.Name = "button4";
+            button4.Size = new Size(152, 23);
+            button4.TabIndex = 5;
+            button4.Text = "Annulla";
+            button4.UseVisualStyleBackColor = true;
+            button4.Click += button4_Click;
+            // 
+            // listBox1
+            // 
+            listBox1.Dock = DockStyle.Fill;
+            listBox1.FormattingEnabled = true;
+            listBox1.ItemHeight = 15;
+            listBox1.Location = new Point(3, 321);
+            listBox1.Name = "listBox1";
+            listBox1.Size = new Size(794, 267);
+            listBox1.TabIndex = 6;
+            listBox1.SelectedIndexChanged += listBox1_SelectedIndexChanged;
+            // 
+            // button5
+            // 
+            button5.Anchor = AnchorStyles.Top | AnchorStyles.Left | AnchorStyles.Right;
+            button5.Location = new Point(161, 3);
+            button5.Name = "button5";
+            button5.Size = new Size(152, 23);
+            button5.TabIndex = 7;
+            button5.Text = "Cancella ultimo elem.";
+            button5.UseVisualStyleBackColor = true;
+            button5.Click += button5_Click;
+            // 
+            // label1
+            // 
+            label1.Anchor = AnchorStyles.None;
+            label1.AutoSize = true;
+            label1.Location = new Point(3, 10);
+            label1.Name = "label1";
+            label1.Size = new Size(67, 15);
+            label1.TabIndex = 8;
+            label1.Text = "Total items:";
+            label1.Click += label1_Click;
+            // 
+            // label2
+            // 
+            label2.Anchor = AnchorStyles.Left | AnchorStyles.Right;
+            label2.AutoSize = true;
+            label2.Location = new Point(77, 10);
+            label2.Name = "label2";
+            label2.Size = new Size(42, 15);
+            label2.TabIndex = 9;
+            label2.Text = "0";
+            label2.Click += label2_Click;
+            // 
+            // tableLayoutPanel1
+            // 
+            tableLayoutPanel1.AutoSize = true;
+            tableLayoutPanel1.ColumnCount = 1;
+            tableLayoutPanel1.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 50F));
+            tableLayoutPanel1.Controls.Add(textBox1, 0, 0);
+            tableLayoutPanel1.Controls.Add(tableLayoutPanel2, 0, 1);
+            tableLayoutPanel1.Controls.Add(tableLayoutPanel3, 0, 2);
+            tableLayoutPanel1.Controls.Add(listBox1, 0, 4);
+            tableLayoutPanel1.Controls.Add(tableLayoutPanel4, 0, 3);
+            tableLayoutPanel1.Dock = DockStyle.Fill;
+            tableLayoutPanel1.Location = new Point(0, 0);
+            tableLayoutPanel1.Name = "tableLayoutPanel1";
+            tableLayoutPanel1.RowCount = 5;
+            tableLayoutPanel1.RowStyles.Add(new RowStyle(SizeType.Percent, 47.8873253F));
+            tableLayoutPanel1.RowStyles.Add(new RowStyle(SizeType.Percent, 52.1126747F));
+            tableLayoutPanel1.RowStyles.Add(new RowStyle(SizeType.Absolute, 42F));
+            tableLayoutPanel1.RowStyles.Add(new RowStyle(SizeType.Absolute, 84F));
+            tableLayoutPanel1.RowStyles.Add(new RowStyle(SizeType.Absolute, 272F));
+            tableLayoutPanel1.Size = new Size(800, 591);
+            tableLayoutPanel1.TabIndex = 10;
+            // 
+            // tableLayoutPanel2
+            // 
+            tableLayoutPanel2.ColumnCount = 5;
+            tableLayoutPanel2.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 20F));
+            tableLayoutPanel2.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 20F));
+            tableLayoutPanel2.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 20F));
+            tableLayoutPanel2.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 20F));
+            tableLayoutPanel2.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 20F));
+            tableLayoutPanel2.Controls.Add(button1, 4, 0);
+            tableLayoutPanel2.Controls.Add(button2, 3, 0);
+            tableLayoutPanel2.Controls.Add(button3, 2, 0);
+            tableLayoutPanel2.Controls.Add(button5, 1, 0);
+            tableLayoutPanel2.Controls.Add(button4, 0, 0);
+            tableLayoutPanel2.Dock = DockStyle.Fill;
+            tableLayoutPanel2.Location = new Point(3, 95);
+            tableLayoutPanel2.Name = "tableLayoutPanel2";
+            tableLayoutPanel2.RowCount = 1;
+            tableLayoutPanel2.RowStyles.Add(new RowStyle(SizeType.Percent, 100F));
+            tableLayoutPanel2.Size = new Size(794, 94);
+            tableLayoutPanel2.TabIndex = 2;
+            // 
+            // tableLayoutPanel3
+            // 
+            tableLayoutPanel3.ColumnCount = 4;
+            tableLayoutPanel3.ColumnStyles.Add(new ColumnStyle(SizeType.Absolute, 74F));
+            tableLayoutPanel3.ColumnStyles.Add(new ColumnStyle(SizeType.Absolute, 48F));
+            tableLayoutPanel3.ColumnStyles.Add(new ColumnStyle(SizeType.Absolute, 573F));
+            tableLayoutPanel3.ColumnStyles.Add(new ColumnStyle(SizeType.Absolute, 20F));
+            tableLayoutPanel3.Controls.Add(label1, 0, 0);
+            tableLayoutPanel3.Controls.Add(label2, 1, 0);
+            tableLayoutPanel3.Dock = DockStyle.Fill;
+            tableLayoutPanel3.Location = new Point(3, 195);
+            tableLayoutPanel3.Name = "tableLayoutPanel3";
+            tableLayoutPanel3.RowCount = 1;
+            tableLayoutPanel3.RowStyles.Add(new RowStyle(SizeType.Percent, 100F));
+            tableLayoutPanel3.Size = new Size(794, 36);
+            tableLayoutPanel3.TabIndex = 7;
+            // 
+            // tableLayoutPanel4
+            // 
+            tableLayoutPanel4.ColumnCount = 5;
+            tableLayoutPanel4.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 20F));
+            tableLayoutPanel4.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 20F));
+            tableLayoutPanel4.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 20F));
+            tableLayoutPanel4.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 20F));
+            tableLayoutPanel4.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 20F));
+            tableLayoutPanel4.ColumnStyles.Add(new ColumnStyle(SizeType.Absolute, 20F));
+            tableLayoutPanel4.ColumnStyles.Add(new ColumnStyle(SizeType.Absolute, 20F));
+            tableLayoutPanel4.Controls.Add(button6, 0, 0);
+            tableLayoutPanel4.Controls.Add(label3, 1, 0);
+            tableLayoutPanel4.Controls.Add(label4, 3, 0);
+            tableLayoutPanel4.Controls.Add(numericUpDown1, 2, 0);
+            tableLayoutPanel4.Controls.Add(numericUpDown2, 4, 0);
+            tableLayoutPanel4.Dock = DockStyle.Fill;
+            tableLayoutPanel4.Location = new Point(3, 237);
+            tableLayoutPanel4.Name = "tableLayoutPanel4";
+            tableLayoutPanel4.RowCount = 1;
+            tableLayoutPanel4.RowStyles.Add(new RowStyle(SizeType.Percent, 100F));
+            tableLayoutPanel4.Size = new Size(794, 78);
+            tableLayoutPanel4.TabIndex = 8;
+            // 
+            // button6
+            // 
+            button6.Location = new Point(3, 3);
+            button6.Name = "button6";
+            button6.Size = new Size(75, 23);
+            button6.TabIndex = 8;
+            button6.Text = "Gen Key(s)";
+            button6.UseVisualStyleBackColor = true;
+            button6.Click += button6_Click;
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new Point(161, 0);
+            label3.Name = "label3";
+            label3.Size = new Size(92, 15);
+            label3.TabIndex = 9;
+            label3.Text = "Nr. of key chars:";
+            label3.Click += label3_Click;
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Location = new Point(477, 0);
+            label4.Name = "label4";
+            label4.Size = new Size(122, 15);
+            label4.TabIndex = 10;
+            label4.Text = "Nr. of keys generated:";
+            // 
+            // numericUpDown1
+            // 
+            numericUpDown1.Location = new Point(319, 3);
+            numericUpDown1.Minimum = new decimal(new int[] { 1, 0, 0, 0 });
+            numericUpDown1.Name = "numericUpDown1";
+            numericUpDown1.Size = new Size(120, 23);
+            numericUpDown1.TabIndex = 11;
+            numericUpDown1.Value = new decimal(new int[] { 10, 0, 0, 0 });
+            // 
+            // numericUpDown2
+            // 
+            numericUpDown2.Location = new Point(635, 3);
+            numericUpDown2.Minimum = new decimal(new int[] { 1, 0, 0, 0 });
+            numericUpDown2.Name = "numericUpDown2";
+            numericUpDown2.Size = new Size(120, 23);
+            numericUpDown2.TabIndex = 12;
+            numericUpDown2.Value = new decimal(new int[] { 10, 0, 0, 0 });
+            // 
+            // Form1
+            // 
+            AcceptButton = button1;
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            CancelButton = button3;
+            ClientSize = new Size(800, 591);
+            Controls.Add(tableLayoutPanel1);
+            KeyPreview = true;
+            Name = "Form1";
+            Text = "Codio list";
+            Load += Form1_Load;
+            tableLayoutPanel1.ResumeLayout(false);
+            tableLayoutPanel1.PerformLayout();
+            tableLayoutPanel2.ResumeLayout(false);
+            tableLayoutPanel3.ResumeLayout(false);
+            tableLayoutPanel3.PerformLayout();
+            tableLayoutPanel4.ResumeLayout(false);
+            tableLayoutPanel4.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)numericUpDown1).EndInit();
+            ((System.ComponentModel.ISupportInitialize)numericUpDown2).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Button button1;
+        private TextBox textBox1;
+        private Button button2;
+        private Button button3;
+        private Button button4;
+        private ListBox listBox1;
+        private Button button5;
+        private Label label1;
+        private Label label2;
+        private TableLayoutPanel tableLayoutPanel1;
+        private TableLayoutPanel tableLayoutPanel2;
+        private TableLayoutPanel tableLayoutPanel3;
+        private Button button6;
+        private TableLayoutPanel tableLayoutPanel4;
+        private Label label3;
+        private Label label4;
+        private NumericUpDown numericUpDown1;
+        private NumericUpDown numericUpDown2;
+    }
+}
---
+++ Form1.cs
@@ -0,0 +1,225 @@
+using System.Text;
+
+namespace Codio
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        //Init memory var for undo
+        public string? mem1 = null; //Init mem 1 variable for undo action
+        public bool isSingle = false; // Init isSingle bool for single list delete undo
+
+        private int _list_counter = 0;
+
+        public event System.EventHandler? VarChanged;
+
+        //On list counter variable change update label
+        protected virtual void OnVarChanged()
+        {
+            if (VarChanged != null) VarChanged(this, EventArgs.Empty);
+            label2.Text = ListCounter.ToString();
+        }
+
+        //Defining List counter get, set
+        public int ListCounter
+        {
+            get
+            {
+                return _list_counter;
+            }
+            set
+            {
+                _list_counter = value;
+                OnVarChanged();
+
+            }
+        }
+
+        //Creating array for undo action of listBox1
+        public string[]? list_content;
+
+        //Todo at form load
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            this.BackColor = Color.LightBlue;  // Changing inner bg color of the window
+            listBox1.MouseDown += ListBox1_MouseDown;
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        //Submit button
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string text_parsed; // Define parsified text variable
+            text_parsed = textBox1.Text;  // Storing var from texBox1
+            if (text_parsed != "")
+            {
+                listBox1.Items.Add(text_parsed); // Add parsed text into listBox1
+                ListCounter++; // Inc counter
+            }
+            textBox1.Clear();
+        }
+
+        private void textBox2_TextChanged_1(object sender, EventArgs e)
+        {
+
+        }
+
+        //Delete all button
+        private void button2_Click(object sender, EventArgs e)
+        {
+            isSingle = false;
+            mem1 = textBox1.Text;
+            list_content = listBox1.Items.OfType<string>().ToArray(); // Storing all data form listBox1 for undo action
+            textBox1.Clear();
+            listBox1.Items.Clear();
+            ListCounter = 0;
+        }
+
+        //Delete upper text
+        private void button3_Click(object sender, EventArgs e)
+        {
+            mem1 = textBox1.Text; // Storing into mem1 what typed in textBox1
+            textBox1.Clear();
+        }
+
+
+        //Undo last action
+        private void button4_Click(object sender, EventArgs e)
+        {
+            if ((mem1 != null) && (list_content != null) && (isSingle == false))
+            {
+                textBox1.AppendText(mem1);
+                mem1 = null;
+                for (int i = 0; i < list_content.Length; i++) // Instert stored mem array back to lisBox1
+                {
+                    listBox1.Items.Add(list_content[i].ToString());
+                    ListCounter++;
+                }
+                list_content = null;
+            }
+            else if ((mem1 != null) && (list_content != null) && (isSingle = true))
+            {
+                textBox1.AppendText(mem1);
+                mem1 = null;
+                listBox1.Items.Add(list_content[ListCounter].ToString()); // Instert stored mem last array item back to lisBox1
+                ListCounter++;
+                list_content = null;
+            }
+            else if ((list_content != null) && (isSingle == false))
+            {
+                for (int i = 0; i < list_content.Length; i++) // Instert stored mem array back to lisBox1
+                {
+                    listBox1.Items.Add(list_content[i].ToString());
+                    ListCounter++;
+                }
+                list_content = null;
+            }
+            else if ((list_content != null) && (isSingle = true))
+            {
+                listBox1.Items.Add(list_content[ListCounter].ToString()); // Instert stored mem last array item back to lisBox1
+                ListCounter++;
+                list_content = null;
+            }
+            else if (mem1 != null)
+            {
+                textBox1.AppendText(mem1);
+                mem1 = null;
+            }
+            else
+            {
+                Form2 f2 = new(); // Show error dialog
+                f2.ShowDialog();
+            }
+        }
+
+        //Delete last element added to lisBox1
+        private void button5_Click(object sender, EventArgs e)
+        {
+            if (ListCounter > 0)
+            {
+                isSingle = true;
+                list_content = listBox1.Items.OfType<string>().ToArray();
+                listBox1.Items.RemoveAt(ListCounter - 1);
+                ListCounter--;
+            }
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void ListBox1_MouseDown(object? sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Right)
+            {
+                int index = listBox1.IndexFromPoint(e.Location);
+                if (index != ListBox.NoMatches)
+                {
+                    listBox1.SelectedIndex = index;
+                    if (listBox1.SelectedItem != null)
+                    {
+                        object selectedItemText = listBox1.SelectedItem;
+                        Clipboard.SetText((string)selectedItemText);
+                    }
+                }
+                else
+                {
+                    Form2 f2 = new(); // Show error dialog
+                    f2.ShowDialog();
+                }
+            }
+        }
+
+        static string GenerateKey()
+        {
+            const string chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+            const int keyLength = 10;
+            Random random = new Random();
+
+            StringBuilder keyBuilder = new StringBuilder();
+            for (int i = 0; i < keyLength; i++)
+            {
+                int index = random.Next(chars.Length);
+                keyBuilder.Append(chars[index]);
+            }
+
+            return keyBuilder.ToString();
+        }
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            const int numberOfKeys = 10;
+
+            for (int i = 0; i < numberOfKeys; i++)
+            {
+                string key = GenerateKey();
+                listBox1.Items.Add(key.ToString());
+                ListCounter++;
+            }
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
